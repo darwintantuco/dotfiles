376248997f8caebca4b1da36135dfd9ec11fef06
darwin christopher tantuco <darwinxciii@yahoo.com>
2019-01-30 02:13:00 +0800

    Update

--- rollback.bash
+++ rollback.bash
@@ -1,11 +1,5 @@
 # Auto revert any migration when performing git checkout
 # Assumes all migration are reversible
-#
-# 1. Checks if current directory is a rails project
-# 2. Compares db/migrate of HEAD and the other branch
-# 3. Execute bundle exec rake db:migrate:down for each unique migration id
-# 4. Execute git checkout -- . db/schema.rb
-# 5. Proceed with git checkout
 
 able_to_git_checkout() {
   git diff --shortstat
@@ -24,7 +18,7 @@ rollback() {
   if ! is_git_branch $1; then return; fi
   if ! is_rails_project; then return; fi
   if [ -d "db/migrate" ]; then
-    # Compare HEAD and other branch, get unique migration ids
+    # Compare HEAD and other branch, get unique migration versions
     versions="$(git diff --name-only $(git rev-parse --abbrev-ref $1...HEAD) db/migrate | grep -oE '\d{14}' )"
     # Reverse the order, revert starting from latest migration
     versions="$(echo $versions | awk '{ for (i=NF; i>1; i--) printf("%s ",$i); print $1; }')"
@@ -37,8 +31,6 @@ rollback() {
 }
 
 git() {
-  if [[ $1 == "checkout" ]]; then
-    rollback $2
-  fi
+  if [ $1 == "checkout" ]; then rollback $2; fi
   command git "$@"
 }
